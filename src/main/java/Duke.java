30a7cfe66249de326d1ce21f90ac89b08eb99533
n3wsoldier <n3wsoldier@gmail.com>
2020-08-23 13:06:26 +0800

    Update Duke.java: store input
    1) Store input by the user
    2) Add "list" condition check to list stored inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,20 +8,31 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+        Boolean exit = false;
+        String[] inputs = new String[100];
+        int inputsIndex = 0;
 
         System.out.println("\t____________________________________________________________");
         System.out.println("\tHello! I'm Duke");
         System.out.println("\tWhat can I do for you?");
         System.out.println("\t____________________________________________________________");
-        Scanner in = new Scanner(System.in);
-        Boolean exit = false;
+
         while(!exit){
             String input = in.nextLine();
             if(input.equals("bye")){
                 exit = true;
-            }else{
+            }else if(input.equals("list")){
                 System.out.println("\t____________________________________________________________");
-                System.out.println("\t"+input);
+                for(int i = 0; i < inputsIndex; i++){
+                    System.out.println("\t " + (i+1) +". "+ inputs[i]);
+                }
+                System.out.println("\t____________________________________________________________");
+            }else{
+                inputs[inputsIndex] = input;
+                inputsIndex++;
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\tadded: "+input);
                 System.out.println("\t____________________________________________________________");
             }
 
