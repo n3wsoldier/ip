cd29d22ac65bbb5675af0725373e22ab322f5860
n3wsoldier <n3wsoldier@gmail.com>
2020-09-20 13:23:14 +0800

    Add DateTimeValidator.java: interface that convert due time
    1) To be implemented by Deadline and Event class
    2) Check for valid date format and convert

--- src/main/java/duke/data/task/Deadline.java
+++ src/main/java/duke/data/task/Deadline.java
@@ -1,13 +1,19 @@
 package duke.data.task;
 
-public class Deadline extends Task {
+import java.text.ParseException;
+import java.util.Date;
+
+public class Deadline extends Task implements DateTimeValidator {
     protected String by;
+    protected boolean isDateString;
+    protected Date byDate;
 
     public Deadline(String description, String by){
         super(description, TaskType.Deadline);
+        isDateString = false;
+        byDate = null;
         this.by = by;
-        this.toString();
-
+        parseToDate(by);
     }
 
     public String getBy(){
@@ -20,6 +26,23 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + (isDateTime() ? dateToString.format(byDate) : by) + ")";
+    }
+
+
+    @Override
+    public void parseToDate(String input) {
+        try {
+            byDate = stringToDate.parse(input);
+            isDateString=  true;
+        } catch (ParseException e) {
+            isDateString=  false;
+        }
+
+    }
+
+    @Override
+    public boolean isDateTime(){
+        return isDateString;
     }
 }
